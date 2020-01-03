4b3aba6fc6915ffe3fe6c87c65ee744924003712
ZC <zhoucan1999@outlook.com>
2020-01-03 18:28:08 +0000

    Create Test01.java

---
+++ src/Test/Test01.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class Test01 {
+	
+	public static void main(String[] args) {
+		String a ="asd";
+		String b = "asd";
+		System.out.println("a==b: " + a==b);
+		System.out.println("a.equals(b):" + a.equals(b));
+	}
+}
