8e4a8b744468f980029f420569594e152b510e6d
ZC <zhoucan1999@outlook.com>
2020-01-03 18:40:31 +0000

    Create Subsets.java

---
+++ src/Backtracking/Subsets/Subsets.java
@@ -0,0 +1,30 @@
+package Backtracking.Subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+	public static void main(String[] args) {
+		int[] input = new int[] {1,2,3};
+		List<List<Integer>> output = subsets(input);
+		for(List<Integer> ls : output) {
+			System.out.println(ls);
+		}
+	}
+	
+	private static List<List<Integer>> subsets(int[] nums) {
+		
+		List<List<Integer>> res = new ArrayList<>();
+		backtrack(0, nums, res, new ArrayList<Integer>());
+		return res;
+	}
+	
+	private static void backtrack(int start, int[] nums, List<List<Integer>> res, List<Integer> tempList) {
+		res.add(new ArrayList<Integer>(tempList));
+		for(int i=start; i<nums.length; i++) {
+			tempList.add(nums[i]);
+			backtrack(i+1, nums, res, tempList);
+			tempList.remove(tempList.size()-1);
+		}
+	}
+}
